7dafceecc29646ef9e13bc7fa4843ee0b81a3fca
albertvaka <albertvaka@gmail.com>
2024-12-01 16:33:33 +0000

    Deploying to main from @ albertvaka/nbgf@6cd7c4263ad96a26d37d8690dbda3bfd6dbe3b72 🚀

--- nbgf/recoil-rider/debug/gaem.js
+++ nbgf/recoil-rider/debug/gaem.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp_u0004c_.js
+// include: /tmp/tmphmwt55jj.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -219,7 +219,7 @@ Module['FS_createPath']("/", "data", true, true);
 
   })();
 
-// end include: /tmp/tmp_u0004c_.js
+// end include: /tmp/tmphmwt55jj.js
 
 
 // Sometimes an existing Module object exists with properties

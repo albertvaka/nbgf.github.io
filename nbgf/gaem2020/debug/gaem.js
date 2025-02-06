8f1895ed574f589aa280dfe0a4761758c33bfc2b
albertvaka <albertvaka@gmail.com>
2025-02-06 15:48:01 +0000

    Deploying to main from @ albertvaka/nbgf@71af85d922f43fa6ab1da7a0f034505ac4da9ae0 🚀

--- nbgf/gaem2020/debug/gaem.js
+++ nbgf/gaem2020/debug/gaem.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpvmz728xj.js
+// include: /tmp/tmpj8qc89vh.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -215,7 +215,7 @@ Module['FS_createPath']("/", "data", true, true);
 
   })();
 
-// end include: /tmp/tmpvmz728xj.js
+// end include: /tmp/tmpj8qc89vh.js
 
 
 // Sometimes an existing Module object exists with properties
